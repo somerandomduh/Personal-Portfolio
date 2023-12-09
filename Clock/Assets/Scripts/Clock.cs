af6b83da4b0c23e83cc1a6a6aab71e4c5ff40586
somerandomduh <52752073+somerandomduh@users.noreply.github.com>
2023-12-09 21:49:11 +0100

    Update for windows editor and finished clock
    I have to re-position the hands of the clock

--- Clock/Assets/Scripts/Clock.cs
+++ Clock/Assets/Scripts/Clock.cs
@@ -12,12 +12,30 @@ public class Clock : MonoBehaviour
 
     [SerializeField]
     Transform secondsPivot;*/
-
-    void Awake() 
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+    void Awake()
     {
+        // Debug.Log(DateTime.Now);
 
-        Debug.Log(DateTime.now);
-        hoursPivot.localRotation = Quaternion.Euler(0,0,-30);
+        DateTime time = DateTime.Now;
+        hoursPivot.localRotation =
+            Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
+        minutesPivot.localRotation =
+            Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
+        secondsPivot.localRotation =
+            Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
     }
-    
+
+
+    void Update()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation =
+            Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
+        minutesPivot.localRotation =
+            Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+        secondsPivot.localRotation =
+            Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
+    }
+
 }
